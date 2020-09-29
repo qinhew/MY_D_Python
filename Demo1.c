5a2a638fbbdee488dd0296b4383c9d12225ff865
wqinhe <270240626@qq.com>
2020-09-29 20:03:45 +0800

    2020.09.29

---
+++ Demo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("%f\n", 1.2);
+    printf("%i\n", 3);
+    printf("%s\n", "d00");
+    
+    system("pause");
+    return 0;
+}
---
+++ Demo2.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: wqinhe
+ * Date: 2020-09-29
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world";
+    
+    system("pause");
+    return 0;
+}
